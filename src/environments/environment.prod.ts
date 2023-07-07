4326adc5ef9504444f096dffb4666eb68e3137ba
Prashant Deshpande <prashant.deshpande@lyveglobal.com>
2023-07-07 11:13:23 +0530

    .envfile changed for Prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: false,
+  baseUrl: 'https://mycontacts-backend-7327.onrender.com',
+  loginUrl: 'https://mycontacts-backend-7327.onrender.com/api/users/login',
+  registerUrl:
+    'https://mycontacts-backend-7327.onrender.com/api/users/register',
+  getContactsUrl: 'https://mycontacts-backend-7327.onrender.com/api/contacts',
+  createContactUrl: 'https://mycontacts-backend-7327.onrender.com/api/contacts',
+  getUserInfo:'https://mycontacts-backend-7327.onrender.com/api/users/currentuser'
 };
